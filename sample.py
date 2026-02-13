1b5651214967e95657a84ce94118894ab7e95354
Laksh <lakshsd476@gmail.com>
2026-02-13 10:48:37 +0530

    Added

--- sample.py
+++ sample.py
@@ -1,25 +1,11 @@
 def add(a, b):
-    """Function `add`.
-
-Args:
-    a, b: input parameters.
-
-Returns:
-    Result of the function."""
+   
     return a + b
 
 
 class Calculator:
-    """Class `Calculator`.
 
-Represents a custom object."""
     
     def multiply(self, x, y):
-        """Function `multiply`.
-
-Args:
-    self, x, y: input parameters.
-
-Returns:
-    Result of the function."""
+  
         return x * y
